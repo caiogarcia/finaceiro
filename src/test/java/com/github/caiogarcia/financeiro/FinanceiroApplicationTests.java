0e02610886e0f0a1f29afd12d3a36fdf8ed18a0b
Caio Garcia <garcia.caio@gmail.com>
2020-08-29 15:44:53 -0300

    Criação do projeto

---
+++ src/test/java/com/github/caiogarcia/financeiro/FinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.caiogarcia.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
